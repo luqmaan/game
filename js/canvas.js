5ef479764c45a95fd61a83f008fa890727a6a515
radicalcakes <tbhuynh@dessertly.com>
2013-03-14 02:45:35 -0400

    added mobile view. todo: draw things to context/images. figure out sprite loading/tojson

--- js/canvas.js
+++ js/canvas.js
@@ -1,27 +1,39 @@
-var canvas = null;
-var ctx = null;
-var img = null;
+// setup object
+var setup = {
+	//some stuff for mobile
+	WIDTH: 320;
+	HEIGHT: 480;
+	RATIO: null;
+	currWidth: null;
+	currHeight: null;
+	canvas: null;
+	ctx: null;
 
-var onImageLoad = function(){
-	console.log("Image loaded");
-	ctx.drawImage(img, 200,200);
+	init: function(){
+		this.RATIO = this.WIDTH/this.HEIGHT;
+		this.currWidth = this.WIDTH;
+		this.currHeight = this.HEIGHT;
+		this.canvas = document.getElementById("bg");
+		this.canvas.width = this.WIDTH;
+		this.canvas.height = this.HEIGHT;
+		this.ctx = canvas.getContext("2d");
+		this.resize();
+	};
+
+	resize: function(){
+		this.currHeight = window.innerHeight;
+		this.currHeight = this.currHeight * this.RATIO;
+		//resize in css
+		this.canvas.style.width = this.currentWidth + 'px';
+	    this.canvas.style.height = this.currentHeight + 'px';
+
+	    //sometimes events don't fire if there isn't a timeout 
+	    //for mobile browsers
+	    window.setTimeout(function() {
+	                window.scrollTo(0,1);
+	    }, 1);
+
+	};
 };
-
-var setup = function(){
-	var body = document.getElementById("mycanvas");
-	canvas = document.createElement("canvas");
-
-	ctx = canvas.getContext("2d");
-
-	canvas.setAttribute("width", 400);
-	canvas.setAttribute("height", 400);
-
-	body.appendChild(canvas);
-
-	img = new Image();
-	img.onload = onImageLoad();
-	img.src = "tilesets/BombermanDojo.png";
-
-};
-
-setup();
+window.addEventListener('load', setup.init, false);
+window.addEventListener('resize', setup.resize, false);
