fa560eed77c7d6a62e6bc228a4614b64b17461af
Luqmaan <ldawoodjee@gmail.com>
2013-03-17 00:52:08 -0400

    Converted grid to a class

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 var imageAssets = ["images/explosion.png", "images/tileset.png", "images/char_gold.png", "images/char_silver.png", "images/atlas.png"];
-var scene, gameLoop, objects = {}, ticker, input, obstacles = [];
+var scene, gameLoop, objects = {}, ticker, input, grid;
 var gameWidth = 15,
     gameHeight = 10;
 
@@ -25,9 +25,13 @@ window.onload = function() {
             useCanvas: 'true'
         });
 
+        grid = new Grid();
+
         // npc = new Person(foreground, 0, 0);
-        var user = new Player(foreground, 3, 2);
+        user = new Player(foreground, 3, 2);
         var w;
+
+        w = new Wall(foreground, 1, 1);
         w = new Wall(foreground, 5, 5);
         w = new Wall(foreground, 6, 5);
         w = new Wall(foreground, 5, 4);
@@ -41,50 +45,191 @@ window.onload = function() {
         w = new Wall(foreground, 2, 3);
         w = new Wall(foreground, 1, 4);
         w = new Wall(foreground, 1, 5);
-        var b = new Bomb(foreground, 2, 1, 5);
+        var b = new Bomb(foreground, 1, 2, 2);
 
         ticker.run();
 
     });
 
-    // populate 2d obstacles array with zeros
-    var row = [];
-    for (var i = 0; i < gameWidth; i++)
-        row[i] = 0;
-    for (i = 0; i < gameHeight; i++)
-        obstacles[i] = row.slice(0);
-
 };
 
 function gameLoop() {
 
-    for (var i in objects) {
-        objects[i].update();
-    }
+    for (var i in objects)
+        if (i in objects)
+            objects[i].update();
 
 }
 
-function canMoveTo(object, xDelta, yDelta) {
+function Grid() {
 
-    var newX = object.xGrid + xDelta;
-    var newY = object.yGrid + yDelta;
+    Array.prototype.clone = function() {
+        var arr = this.slice(0);
+        for( var i = 0; i < this.length; i++ ) {
+            if( this[i].clone ) {
+                //recursion
+                arr[i] = this[i].clone();
+            }
+        }
+        return arr;
+    };
 
-    if (newX >= 0 && newY >= 0 && newX < gameWidth && newY < gameHeight) {
-        console.log("obstacles[" + newY + "][" + newX + "] = " + obstacles[newY][newX]);
-        if (obstacles[newY][newX] === 0 || obstacles[newY][newX].type === "person")
+    // add contains function
+    Array.prototype.contains = function(type) {
+        var t = Object(this);
+        for (var i = 0; i < t.length; t++) {
+            if (i in t) {
+                if (t[i].type === type)
+                    return true;
+            }
+        }
+        return false;
+    };
+
+    // add filter function
+    if (!Array.prototype.filter) {
+        Array.prototype.filter = function(fun /*, thisp */ ) {
+            "use strict";
+
+            if (this === null) throw new TypeError();
+
+            var t = Object(this);
+            var len = t.length >>> 0;
+            if (typeof fun !== "function") throw new TypeError();
+
+            var res = [];
+            var thisp = arguments[1];
+            for (var i = 0; i < len; i++) {
+                if (i in t) {
+                    var val = t[i]; // in case fun mutates this
+                    if (fun.call(thisp, val, i, t)) res.push(val);
+                }
+            }
+
+            return res;
+        };
+    }
+
+    var self = [];
+
+    // populate 2d grid array with empty arrays
+    var row = [];
+    for (var i = 0; i < gameWidth; i++) {
+        row[i] = [];
+    }
+    for (var i = 0; i < gameHeight; i++) {
+        self[i] = row.clone();
+    }
+
+    // Arguments can be entity or x, y. 
+    // If entity, returns the other entites occupying the same position.
+    self.at = function() {
+        if (typeof arguments[0] === "object") {
+            var entity = arguments[0];
+            return grid[entity.yGrid][entity.xGrid].filter(function (element, index, array) {
+                return element !== entity;
+            });
+        } else {
+            console.log("called grid["+y+"]["+x+"]");
+            var x = arguments[0];
+            var y = arguments[1];
+            if (x >= gameWidth || y >= gameHeight)
+                return [];
+            return grid[y][x];
+        }
+    };
+
+    self.add = function(object) {
+        if (self.canMoveTo(object, object.xGrid, object.yGrid)) {
+            grid[object.yGrid][object.xGrid].push({
+                type: object.type,
+                hash: object.hash
+            });
+        }
+        else {
+            console.error("Cannot add " + object.type + " to ("+object.xGrid+","+object.yGrid+")");
+        }
+    };
+
+    self.remove = function(object) {
+        grid[object.yGrid][object.xGrid] = grid[object.yGrid][object.xGrid].filter(function (element, index, array) {
+            return element.hash !== object.hash;
+        });
+    };
+
+    self.moveTo = function(object, x, y) {
+        if (self.canMoveTo(object, x, y)) {
+            self.remove(object);
+            object.xGrid = x;
+            object.yGrid = y;
+            self.add(object, x, y);
             return true;
-    }
-    return false;
+        }
+        else
+            return false;
+    };
+
+    self.moveBy = function(object, xDelta, yDelta) {
+        var newX = object.xGrid + xDelta;
+        var newY = object.yGrid + yDelta;
+        if (self.canMoveTo(object, newX, newY)) {
+            self.remove(object);
+            object.xGrid = newX;
+            object.yGrid = newY;
+            grid[object.yGrid][object.xGrid].push({
+                type: object.type,
+                hash: object.hash
+            });
+            return true;
+        }
+        else
+            return false;
+    };
+
+    self.canMoveTo = function(object, x, y) {
+        if (x < 0 || y < 0 || x >= gameWidth || y >= gameHeight) {
+            return false;
+        }
+        else if (grid[y][x].contains("bomb") || grid[y][x].contains("wall")) {
+            return false;
+        }
+        return true;
+    };
+
+    self.dump = function() {
+
+        for (var i = 0; i < self.length; i++) {
+            var r = i + " | ";
+            for (var j = 0; j < self[i].length; j++) {
+                if (self[i][j].length > 0) {
+                    for (var k = 0; k < self[i][j].length; k++) {
+                        r += " "+j+"="+self[i][j][k].type;
+                    }
+                }
+                else {
+                    r += " "+j+"=_";
+                }
+            }
+            console.log(r);
+        }
+
+    };
+
+    return self;
+
 }
+
+
 
 function Entity(xGrid, yGrid, type) {
+    console.log("new " + type + " at (" + xGrid + "," + yGrid + ")");
     var self = this;
     self.xGrid = xGrid;
     self.yGrid = yGrid;
     self.hash = hashify();
-    self.updateObstaclePosition = function() {
-        obstacles[self.yGrid][self.xGrid] = {type: type, hash: self.hash};
-    };
+    self.type = type;
+    grid.add(self);
+    objects[self.hash] = self;
 }
 
 function Person(layer, xGrid, yGrid) {
@@ -98,18 +243,20 @@ function Person(layer, xGrid, yGrid) {
         yscale: 1,
         xoffset: 4,
         yoffset: 5,
-        rv: Math.PI/8
+        rv: Math.PI / 8
     };
     self.sprite = scene.Sprite("images/char_gold.png", options);
     self.update = function() {
         self.sprite.update();
     };
     self.die = function() {
-        self.sprite.setX(0);
-        self.sprite.setY(0);
-        self.xGrid = 0;
-        self.yGrid = 0;
-        self.updateObstaclePosition();
+        if (grid.moveTo(self, 0, 0)) {
+            console.log("yes can move there")
+            self.sprite.setX(0);
+            self.sprite.setY(0);
+        } else {
+            console.error("uhoh can't move there");
+        }
         self.sprite.setOpacity(0.5);
     };
     self.move = function(direction) {
@@ -136,22 +283,14 @@ function Person(layer, xGrid, yGrid) {
         default:
             break;
         }
-
-        if (canMoveTo(self, x, y)) {
-            obstacles[self.yGrid][self.xGrid] = 0;
-            self.xGrid += x;
-            self.yGrid += y;
-            self.updateObstaclePosition();
-            console.log("self.xGrid " + self.xGrid + " self.yGrid " + self.yGrid);
+        if (grid.moveBy(self, x, y)) {
             x *= 32;
-            y *= 32;   
+            y *= 32;
             self.sprite.move(x, y);
         }
         self.sprite.update();
     };
     self.update();
-    objects[self.hash] = self;
-    self.updateObstaclePosition();
     return self;
 }
 
@@ -178,7 +317,7 @@ function Player(layer, xGrid, yGrid) {
 
 function Wall(layer, xGrid, yGrid) {
 
-    var self = new Entity(xGrid, yGrid);
+    var self = new Entity(xGrid, yGrid, "wall");
     self.sprite = scene.Sprite("images/tileset.png", {
         layer: layer,
         x: (xGrid * 32),
@@ -192,23 +331,18 @@ function Wall(layer, xGrid, yGrid) {
 
     self.damage = function() {
         self.health -= 1;
-        if (self.health === 0)
-            self.destroy();
+        if (self.health === 0) self.destroy();
     };
-
-    obstacles[yGrid][xGrid] = {type: "wall", hash: self.hash};
 
     self.update = function() {
         self.sprite.update();
     };
     self.destroy = function() {
-        obstacles[self.yGrid][self.xGrid] = 0;
+        grid.remove(self);
         delete objects[self.hash];
         self.sprite.remove();
     };
 
-    self.sprite.setBackgroundRepeat("repeat");
-    objects[self.hash] = self;
     self.update();
     return self;
 }
@@ -229,7 +363,9 @@ function Bomb(layer, xGrid, yGrid, radius) {
     });
     self.radius = radius;
 
-    var iX = 2, iY = 0, steps = 1;
+    var iX = 2,
+        iY = 0,
+        steps = 1;
     self.update = function() {
         self.sprite.update();
         self.sprite.setXOffset(iX++ * 66);
@@ -237,8 +373,7 @@ function Bomb(layer, xGrid, yGrid, radius) {
         if (steps === 5) {
             iY++;
             iX = 0;
-        }
-        else if (steps === 9) {
+        } else if (steps === 9) {
             self.explode();
         }
         steps++;
@@ -256,7 +391,7 @@ function Bomb(layer, xGrid, yGrid, radius) {
             // console.log("self.yGrid - r = " + (self.yGrid - r <= 1) )
             newVal = self.yGrid - r;
             if (newVal >= 0) {
-                if (r === self.radius || newVal === 0 || obstacles[newVal][self.xGrid] === 1) {
+                if (r === self.radius || newVal === 0 || grid.at(newVal,self.xGrid).contains("wall")) {
                     f = new Fire(layer, self.xGrid, newVal, -4);
                     break;
                 } else {
@@ -268,7 +403,7 @@ function Bomb(layer, xGrid, yGrid, radius) {
         for (r = 1; r <= self.radius; r++) {
             newVal = self.yGrid + r;
             if (newVal < gameHeight) {
-                if (r === self.radius || newVal === gameHeight-1 || obstacles[newVal][self.xGrid] === 1) {
+                if (r === self.radius || newVal === gameHeight - 1 || grid.at(newVal,self.xGrid).contains("wall")) {
                     f = new Fire(layer, self.xGrid, newVal, 4);
                     break;
                 } else {
@@ -280,7 +415,7 @@ function Bomb(layer, xGrid, yGrid, radius) {
         for (r = 1; r <= self.radius; r++) {
             newVal = self.xGrid - r;
             if (newVal >= 0) {
-                if (r === self.radius || newVal === 1 || obstacles[self.yGrid][newVal] === 1) {
+                if (r === self.radius || newVal === 1 || grid.at(self.yGrid,newVal).contains("wall")) {
                     f = new Fire(layer, newVal, self.yGrid, -2);
                     break;
                 } else {
@@ -292,7 +427,7 @@ function Bomb(layer, xGrid, yGrid, radius) {
         for (r = 1; r <= self.radius; r++) {
             newVal = self.xGrid + r;
             if (newVal < gameWidth) {
-                if (r === self.radius || newVal === gameWidth-1 || obstacles[self.yGrid][newVal] === 1) {
+                if (r === self.radius || newVal === gameWidth - 1 || grid.at(self.yGrid,newVal).contains("wall")) {
                     f = new Fire(layer, newVal, self.yGrid, 2);
                     break;
                 } else {
@@ -301,20 +436,18 @@ function Bomb(layer, xGrid, yGrid, radius) {
             }
         }
 
-
-
         self.destroy();
+
     };
+
     self.destroy = function() {
-        obstacles[self.yGrid][self.xGrid] = 0;
+        grid.remove(self);
         delete objects[self.hash];
         self.sprite.remove();
     };
 
-    objects[self.hash] = self;
-    self.updateObstaclePosition();
-
     self.update();
+
 }
 /**
  * Fire class
@@ -325,7 +458,8 @@ function Bomb(layer, xGrid, yGrid, radius) {
  */
 function Fire(layer, xGrid, yGrid, type) {
     var self = new Entity(xGrid, yGrid, "fire");
-    var scale = 1, angle = 0;
+    var scale = 1,
+        angle = 0;
     if (type < 0) {
         type *= -1;
         scale = -1;
@@ -349,7 +483,6 @@ function Fire(layer, xGrid, yGrid, type) {
         "xscale": scale,
         "yscale": scale
     });
-    console.log(angle);
 
     self.lifetime = 2;
     self.createdOn = ticker.currentTick;
@@ -357,31 +490,34 @@ function Fire(layer, xGrid, yGrid, type) {
     self.update = function() {
         i = i === 1 ? 1 : 0;
         self.sprite.setYOffset(i++ * 32);
-        if (ticker.currentTick - self.createdOn >= self.lifetime)
-            self.destroy();
-        else
-            self.sprite.update();
+        if (ticker.currentTick - self.createdOn >= self.lifetime) self.destroy();
+        else self.sprite.update();
     };
     self.destroy = function() {
-        // console.log('obstacles['+self.yGrid+']['+self.xGrid+']');
+        // console.log('grid['+self.yGrid+']['+self.xGrid+']');
         // kill or damage people and walls
-        var o = obstacles[self.yGrid][self.xGrid];
-        switch (o.type) {
-        case "person":
-            console.log("About to die person " + o.hash);
-            getHash(o.hash).die();
-            break;
-        case "wall":
-            getHash(o.hash).damage();
-            break;
-        default:
-            break;
+        var intersects = grid.at(self);
+
+        for (var i = 0; i < intersects.length; i++) {
+            var o =  intersects[i];
+            switch (o.type) {
+            case "person":
+                console.log("About to die person " + o.hash);
+                unhash(o.hash).die();
+                break;
+            case "wall":
+                unhash(o.hash).damage();
+                break;
+            default:
+                break;
+            }
         }
 
-        delete objects[self.hash];
+
+        grid.remove(self);
         self.sprite.remove();
+        delete objects[self.hash];
     };
-    objects[self.hash] = self;
     self.update();
 }
 
@@ -390,6 +526,6 @@ function hashify() {
     return Math.ceil(Date.now() + Math.random() * 1000);
 }
 
-function getHash(hash) {
+function unhash(hash) {
     return objects[hash];
 }
